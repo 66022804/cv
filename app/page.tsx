f7aab149cef44da33fc5b2533d0ce253634ad736
Phonsinee <66022804@up.ac.th>
2025-07-07 21:37:44 +0700

    Add service section

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import Image from "next/image";
 
-const sections = ["home", "about", "server", "projects", "contact"];
+const sections = ["home", "about", "service", "projects", "contact"];
 const images = ["/bg.jpg", "/1.jpg", "/2.jpg"];
 
 const smoothScrollTo = (targetId: string, offset = 0) => {
@@ -50,7 +50,7 @@ export default function CV() {
   const navItems = [
     { id: "home", label: "Home" },
     { id: "about", label: "About" },
-    { id: "server", label: "Server" },
+    { id: "service", label: "Service" },
     { id: "projects", label: "Projects" },
     { id: "contact", label: "Contact" },
   ];
@@ -88,6 +88,43 @@ export default function CV() {
     return () => observer.disconnect();
   }, []);
 
+  /* services SECTION */
+  const serviceRef = useRef<HTMLDivElement>(null);
+  const [serviceVisible, setServiceVisible] = useState(false);
+  const [serviceAnimate, setServiceAnimate] = useState(false);
+  const animationTimeout = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    const observice = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setServiceVisible(true);
+          setServiceAnimate(false); // reset ก่อน
+          if (animationTimeout.current) {
+            clearTimeout(animationTimeout.current);
+          }
+          animationTimeout.current = setTimeout(() => {
+            setServiceAnimate(true); // trigger animation
+          }, 10); // ทำให้เร็วขึ้น (50 อาจช้าเกินไป)
+        } else {
+          setServiceVisible(false);
+          setServiceAnimate(false); // reset เมื่อออกนอกจอ
+          if (animationTimeout.current) {
+            clearTimeout(animationTimeout.current);
+            animationTimeout.current = null;
+          }
+        }
+      },
+      { threshold: 0.3 }
+    );
+
+    if (serviceRef.current) observice.observe(serviceRef.current);
+    return () => {
+      if (animationTimeout.current) clearTimeout(animationTimeout.current);
+      observice.disconnect();
+    };
+  }, []);
+
   return (
     <>
       <nav
@@ -110,7 +147,7 @@ export default function CV() {
                   onClick={() =>
                     smoothScrollTo(
                       `#${id}`,
-                      id === "about" ? -240 : id === "server" ? -50 : 0
+                      id === "about" ? -240 : id === "server" ? -55 : 0
                     )
                   }
                   className={`relative xl:text-2xl lg:text-xl md:text-lg sm:text-md cursor-pointer transition-colors duration-200 ${
@@ -299,7 +336,7 @@ export default function CV() {
             <h2 className="lg:text-4xl md:text-3xl sm:text-2xl text-xl font-bold md:mb-3 mb-5 text-center ">
               About
             </h2>
-            <div className="flex flex-col md:flex-row items-center gap-10 max-w-6xl 2xl:mx-auto xl:mx-12 lg:mx-10 md:mx-7 sm:mx-8 mx-4 mb-20">
+            <div className="flex flex-col md:flex-row items-center gap-10 max-w-6xl 2xl:mx-auto xl:mx-12 lg:mx-10 md:mx-7 sm:mx-8 mx-4 ">
               {/* รูปวงกลมด้านซ้าย */}
               <div
                 className={`relative 2xl:w-[750px] 2xl:h-80 xl:w-[650px] xl:h-64 lg:w-[600px] lg:h-52 md:w-[500px] md:h-36 sm:w-[150px] sm:h-36 w-[120px] h-30 rounded-full overflow-hidden shadow-lg ${
@@ -325,7 +362,7 @@ export default function CV() {
                 </p>
 
                 <div className="flex flex-wrap xl:flex-nowrap gap-4">
-                  <p
+                  <div
                     className={`group lg:text-xl md:text-lg sm:text-md text-sm p-2 shadow-lg rounded-2xl flex items-center gap-2 w-fit transition-all duration-300
     bg-gradient-to-r from-[#3abaa8] to-[#232323] bg-clip-text text-transparent
     hover:text-white hover:bg-gradient-to-r hover:from-[#3abaa8] hover:to-[#232323] hover:bg-clip-padding cursor-pointer
@@ -341,9 +378,9 @@ export default function CV() {
                         className="object-contain transition duration-300 group-hover:invert"
                       />
                     </div>
-                  </p>
+                  </div>
 
-                  <p
+                  <div
                     className={`group lg:text-xl md:text-lg sm:text-md text-sm p-2 shadow-lg rounded-2xl flex items-center gap-2 w-fit transition-all duration-300
                       bg-gradient-to-r from-[#3abaa8] to-[#232323] bg-clip-text text-transparent
                       hover:text-white hover:bg-gradient-to-r hover:from-[#3abaa8] hover:to-[#232323] hover:bg-clip-padding cursor-pointer
@@ -359,9 +396,9 @@ export default function CV() {
                         className="object-contain transition duration-300 group-hover:invert"
                       />
                     </div>
-                  </p>
+                  </div>
 
-                  <p
+                  <div
                     className={`group lg:text-xl md:text-lg text-sm p-2 shadow-lg rounded-2xl flex items-center gap-2 w-fit transition-all duration-300
                       bg-gradient-to-r from-[#3abaa8] to-[#232323] bg-clip-text text-transparent
                       hover:text-white hover:bg-gradient-to-r hover:from-[#3abaa8] hover:to-[#232323] hover:bg-clip-padding cursor-pointer
@@ -377,9 +414,9 @@ export default function CV() {
                         className="object-contain transition duration-300 group-hover:invert"
                       />
                     </div>
-                  </p>
+                  </div>
 
-                  <p
+                  <div
                     className={`group lg:text-xl md:text-lg text-sm p-2 shadow-lg rounded-2xl flex items-center gap-2 w-fit transition-all duration-300
                       bg-gradient-to-r from-[#3abaa8] to-[#232323] bg-clip-text text-transparent
                       hover:text-white hover:bg-gradient-to-r hover:from-[#3abaa8] hover:to-[#232323] hover:bg-clip-padding cursor-pointer
@@ -395,9 +432,9 @@ export default function CV() {
                         className="object-contain transition duration-300 group-hover:invert"
                       />
                     </div>
-                  </p>
+                  </div>
 
-                  <p
+                  <div
                     className={`group lg:text-xl md:text-lg text-sm p-2 shadow-lg rounded-2xl flex items-center gap-2 w-fit transition-all duration-300
                       bg-gradient-to-r from-[#3abaa8] to-[#232323] bg-clip-text text-transparent
                       hover:text-white hover:bg-gradient-to-r hover:from-[#3abaa8] hover:to-[#232323] hover:bg-clip-padding cursor-pointer
@@ -413,9 +450,9 @@ export default function CV() {
                         className="object-contain transition duration-300 group-hover:invert"
                       />
                     </div>
-                  </p>
+                  </div>
 
-                  <p
+                  <div
                     className={`group lg:text-xl md:text-lg text-sm p-2 shadow-lg rounded-2xl flex items-center gap-2 w-fit transition-all duration-300
                       bg-gradient-to-r from-[#3abaa8] to-[#232323] bg-clip-text text-transparent
                       hover:text-white hover:bg-gradient-to-r hover:from-[#3abaa8] hover:to-[#232323] hover:bg-clip-padding cursor-pointer
@@ -431,8 +468,128 @@ export default function CV() {
                         className="object-contain transition duration-300 group-hover:invert"
                       />
                     </div>
-                  </p>
+                  </div>
                 </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="service" className="scroll-mt-24 mt-60 px-6">
+          <style>{`
+    @keyframes fade-in-up {
+      0% {
+        opacity: 0;
+        transform: translateY(20px);
+      }
+      100% {
+        opacity: 1;
+        transform: translateY(0);
+      }
+    }
+
+    .fade-in-up {
+      animation: fade-in-up 1.5s ease-out forwards;
+    }
+      @keyframes slide-in-left {
+    0% {
+      opacity: 0;
+      transform: translateX(-50px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateX(0);
+    }
+  }
+
+  @keyframes slide-in-right {
+    0% {
+      opacity: 0;
+      transform: translateX(50px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateX(0);
+    }
+  }
+
+  .slide-in-left {
+    animation: slide-in-left 1s ease-out forwards;
+  }
+
+  .slide-in-right {
+    animation: slide-in-right 1s ease-out forwards;
+  }
+  `}</style>
+
+          <div
+            ref={serviceRef}
+            className={`bg-gradient-to-r from-[#3abaa8] to-[#232323] flex flex-col justify-center text-center 2xl:px-28 2xl:py-36 xl:py-28 xl:px-24 lg:px-20 lg:py-20 md:px-8 md:py-14 sm:px-7 sm:py-10 px-5 py-8 rounded-[2rem] lg:mx-auto sm:mx-10 mx-5 w-fit text-white transition-all duration-700 ease-out ${
+              serviceVisible
+                ? "opacity-100 translate-y-0"
+                : "opacity-0 translate-y-10"
+            }`}
+          >
+            {/* ✅ เพิ่ม class fade-in-up ชัดเจนเมื่อ serverVisible === true */}
+            <h2
+              className={`lg:text-4xl md:text-3xl sm:text-2xl text-xl font-bold lg:mb-4 mb-2 ${
+                serviceAnimate ? "fade-in-up" : ""
+              }`}
+            >
+              Service
+            </h2>
+
+            <p
+              className={`lg::text-xl md:text-lg sm:text-md text-sm lg:pt-10 pt-5 max-w-2xl transition-all duration-700 ${
+                serviceVisible ? "fade-in-up" : ""
+              }`}
+            >
+              HTML/CSS Development: Showcase proficiency in writing clean,
+              semantic HTML markup and CSS stylesheets to create visually
+              appealing and well-structured web pages.
+            </p>
+
+            <div className="flex justify-center 2xl:gap-36 xl:gap-28 lg:gap-24 md:gap-18 sm:gap-14 gap-8 md:pt-10 pt-7">
+              {/* กล่อง 1 */}
+              <div
+                className={`group flex flex-col items-center cursor-pointer bg-gray-100 hover:bg-gray-200 rounded-2xl shadow-lg 2xl:p-6 2xl:px-10 xl:px-8 xl:p-4 lg:px-7 lg:p-3 md:px-6 md:p-2 sm:px-5 sm:p-2 px-3 p-2 w-fit hover:shadow-2xl hover:scale-105 transition duration-300 border border-gray-300 ${
+                  serviceVisible ? "slide-in-left" : "opacity-0"
+                }`}
+              >
+                <div
+                  className="relative 2xl:w-36 2xl:h-36 xl:w-32 xl:h-32 lg:w-28 lg:h-28 md:w-20 md:h-20 sm:w-14 sm:h-18 w-12 h-14
+                "
+                >
+                  <Image
+                    src="/responsive.png"
+                    alt="responsive Icon"
+                    fill
+                    className="object-contain mb-3 transition duration-300"
+                  />
+                </div>
+
+                <span className="text-black lg:text-lg md:text-md sm:text-sm text-xs font-semibold group-hover:text-[#3abaa8] transition">
+                  Responsive
+                </span>
+              </div>
+
+              {/* กล่อง 2 */}
+              <div
+                className={`group flex flex-col items-center cursor-pointer bg-gray-100 hover:bg-gray-200 rounded-2xl shadow-lg 2xl:p-6 2xl:px-10 xl:px-8 xl:p-4 lg:px-7 lg:p-3 md:px-7 md:p-3 sm:px-7 sm:p-3 px-5 p-2 w-fit hover:shadow-2xl hover:scale-105 transition duration-300 border border-gray-300 ${
+                  serviceVisible ? "slide-in-right" : "opacity-0"
+                }`}
+              >
+                <div className="relative 2xl:w-36 2xl:h-36 xl:w-32 xl:h-32 lg:w-28 lg:h-28 md:w-20 md:h-20 sm:w-14 sm:h-14 w-12 h-14">
+                  <Image
+                    src="/Design.png"
+                    alt="Design Icon"
+                    fill
+                    className="object-contain mb-3 transition duration-300"
+                  />
+                </div>
+                <span className="text-black lg:text-lg md:text-md sm:text-sm text-xs mt-2 font-semibold group-hover:text-[#3abaa8] transition">
+                  Design
+                </span>
               </div>
             </div>
           </div>
